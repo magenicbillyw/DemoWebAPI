223009cccd134f119c1733c69231142bae7ccf60
Billy Walton <BillyW@magenic.com>
2021-05-22 21:26:04 -0500

    Add project files.

---
+++ DemoWebAPI/Controllers/CourseController.cs
@@ -0,0 +1,28 @@
+﻿using DemoWebAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoWebAPI.Controllers
+{
+    [ApiController]
+    [Route("/api/Course")]
+    public class CourseController : Controller
+    {
+        private CourseService _courseService = null;
+
+        public CourseController(CourseService courseService)
+        {
+            this._courseService = courseService;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var courses = _courseService.GetCourses();
+            return Ok(courses);
+        }
+    }
+}
---
+++ DemoWebAPI/Services/CourseService.cs
@@ -0,0 +1,28 @@
+﻿using DemoWebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoWebAPI.Services
+{
+    public class CourseService
+    {
+        public List<Course> course_list;
+
+        public CourseService()
+        {
+            course_list = new List<Course>
+            {
+                new Course{ CourseId = 1, CourseName = "AZ-204"},
+                new Course{ CourseId = 2, CourseName = "AZ-304"},
+                new Course{ CourseId = 3, CourseName = "AZ-404"}
+            };
+        }
+    
+        public IEnumerable<Course> GetCourses(){
+            return course_list;
+        }
+    
+    }
+}
